23274a605377edb80086181510ddfc84580ee5f7
Alberto Alvarado Jr <65637580+albertgs09@users.noreply.github.com>
2023-05-19 21:51:44 -0500

    Update AIAttacking.cs

--- Assets/MyFolder/Scripts/AI/AIAttacking.cs
+++ Assets/MyFolder/Scripts/AI/AIAttacking.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class AIAttacking : MonoBehaviour
 {
     private Animator anim;
-    // Start is called before the first frame update
-    void Start()
+    
+    private void Start()
     {
         anim = GetComponentInChildren<Animator>();
     }
